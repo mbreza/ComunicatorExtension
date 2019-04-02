5178020507daebf479ddc5bc6205f810e86adfab
mbreza <mbreza@mbreza.mbreza>
2019-04-02 18:41:15 +0200

    Start of create key logic

--- src/js/popup.js
+++ src/js/popup.js
@@ -3,6 +3,25 @@ import "../css/popup.css";
 var $ = require("jquery");
 var openpgp = require('openpgp');
 
+
+$('#create').click(function(){
+   
+    var options = {
+        userIds: [{ name:$('#name').val(), email:$('#emailaddress').val() }],
+        numBits: 4096,
+        passphrase: $('#password').val()
+    };
+
+    openpgp.generateKey(options).then(function(key) {
+        var privkey = key.privateKeyArmored;
+        var pubkey = key.publicKeyArmored;
+        var revocationCertificate = key.revocationCertificate;
+        chrome.storage.local.set({prywatny: privkey});
+    });
+
+})
+
+/*
 var options = {
     userIds: [{ name:'Jon Smith', email:'jon@example.com' }],
     numBits: 4096,
@@ -20,8 +39,6 @@ chrome.storage.sync.get(['prywatny'], function(result) {
     console.log('Value currently is ' + result.prywatny);
 })
 
-
-/*
 $(function () {
     $('#test1').text("Test2");
 
