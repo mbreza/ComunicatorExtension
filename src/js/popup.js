947eec263e87b59865654f658e56ae205582af10
Mateusz Breza <breza.mateusz@gmail.com>
2019-04-09 22:42:56 +0200

    create key in background

--- src/js/popup.js
+++ src/js/popup.js
@@ -16,63 +16,13 @@ $(function () {
 
 $('#create').click(function(){
 
-    var options = {
-        userIds: [{ name:$('#name').val(), email:$('#emailaddress').val() }],
-        numBits: 4096,
-        passphrase: $('#password').val()
-    };
+    let user = new User($('#name').val(), $('#emailaddress').val(), $('#password').val(),
+                        null, null, null);
 
-    openpgp.generateKey(options).then(function(key) {
-        var privkey = key.privateKeyArmored;
-        var pubkey = key.publicKeyArmored;
-        var revocationCertificate = key.revocationCertificate;
-
-        chrome.storage.local.get(['UserList'], function(result) {
-            let user = new User($('#name').val(), $('#emailaddress').val(), $('#password').val(),
-                                privkey, pubkey, revocationCertificate);
-            
-            var LocalUserList = result.UserList;
-            LocalUserList.push(user);
-            chrome.storage.local.set({UserList: LocalUserList});
-            console.log("nosz kurwa");
-        })
-    });
-    
-})
-
+    chrome.runtime.sendMessage(user);
 /*
-var options = {
-    userIds: [{ name:'Jon Smith', email:'jon@example.com' }],
-    numBits: 4096,
-    passphrase: 'super long and hard to guess secret'
-};
-
-openpgp.generateKey(options).then(function(key) {
-    var privkey = key.privateKeyArmored;
-    var pubkey = key.publicKeyArmored;
-    var revocationCertificate = key.revocationCertificate;
-    chrome.storage.sync.set({prywatny: privkey});
-});
-
-chrome.storage.sync.get(['prywatny'], function(result) {
-    console.log('Value currently is ' + result.prywatny);
-})
-
-$(function () {
-    $('#test1').text("Test2");
-
-})
-
-$(function () {
-    chrome.storage.sync.set({login: 'Mateusz'}, function() {
-        console.log('Value is set to Mateusz');
+    chrome.runtime.sendMessage(user, function(response) {
+        console.log(response);
       });
-});
-
-$('#test2').click(function(){
-    chrome.storage.sync.get(['login'], function(result) {
-        $('#test1').text(result.login);
-        console.log('Value currently is ' + result.login);
-    })
-})
 */
+})
