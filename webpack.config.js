c5ed0f7b0df64e181d5c75a0670926f95f052a77
Mateusz Breza <breza.mateusz@gmail.com>
2019-04-10 22:54:45 +0200

    Adding bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,35 @@ var options = {
     filename: "[name].bundle.js"
   },
   module: {
-    rules: [
+    rules: [  
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      },
       {
         test: /\.css$/,
         loader: "style-loader!css-loader",
